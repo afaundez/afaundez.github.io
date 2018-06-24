15067ce9f79bd7809486b00ca295eafbf487903f
Alvaro Faundez <alvaro@faundez.net>
2018-06-23 23:34:29 -0400

    Clean javascript code into more files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-  let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
-  let active = false;
-
-  const lazyLoad = function() {
-    if (active === false) {
-      active = true;
-
-      setTimeout(function() {
-        lazyImages.forEach(function(lazyImage) {
-          if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
-            lazyImage.src = lazyImage.dataset.src;
-            lazyImage.srcset = lazyImage.dataset.srcset;
-            lazyImage.classList.remove("lazy");
-
-            lazyImages = lazyImages.filter(function(image) {
-              return image !== lazyImage;
-            });
-
-            if (lazyImages.length === 0) {
-              document.removeEventListener("scroll", lazyLoad);
-              window.removeEventListener("resize", lazyLoad);
-              window.removeEventListener("orientationchange", lazyLoad);
-            }
-          }
-        });
-
-        active = false;
-      }, 200);
-    }
-  };
-
-  document.addEventListener("scroll", lazyLoad);
-  window.addEventListener("resize", lazyLoad);
-  window.addEventListener("orientationchange", lazyLoad);
-});
-
-for (var elem of document.querySelectorAll('.materialboxed')) {
+for (let elem of document.querySelectorAll('.materialboxed')) {
   M.Materialbox.init(elem);
 }
 
-const getActiveElement = function(id) {
-  document.getElementById(id);
-};
-
-const options = {
-  getActiveElement,
-  activeClass: 'show-up'
-};
-
-for (elem of document.querySelectorAll('.scrollspy')) {
-  M.ScrollSpy.init(elem, options);
+for (let elem of document.querySelectorAll('.scrollspy')) {
+  M.ScrollSpy.init(elem, { activeClass: 'show-up' });
 }
