f3655e0cb5d53d9d95d398f29fdfc18ce3ddd414
Alvaro Faundez <alvaro@faundez.net>
2018-06-26 01:12:31 -0400

    Refgactor all CSS
    Remove materialize css and js
    Remove unnecessary fonts
    Reorder sass folder

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-for (let elem of document.querySelectorAll('.materialboxed')) {
-  M.Materialbox.init(elem);
-}
-
-for (let elem of document.querySelectorAll('.scrollspy')) {
-  M.ScrollSpy.init(elem, { activeClass: 'show-up' });
-}
